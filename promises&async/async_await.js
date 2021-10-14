f576297ad7d163fe07eda8a4e4fa76b868fb7ea4
Yalchin403 <yalchin.mammadli.std@bhos.edu.az>
2021-10-14 12:25:59 -0700

    aysnc example added
    1st example uses promise, the next uses async, to capture the result of the async we need use .then, otherwise we'll get "promise pending"

--- promises&async/async_await.js
+++ promises&async/async_await.js
@@ -2,20 +2,34 @@
 const fetch = require('node-fetch');
 
 
-function loadJson(url) {
-    return fetch(url)
-        .then(response => {
-            if (response.status === 200) {
-                return response.json();
-            } else {
-                throw new Error(response.status);
-            }
-        });
+// function loadJson(url) {
+//     return fetch(url)
+//         .then(response => {
+//             if (response.status === 200) {
+//                 return response.json();
+//             } else {
+//                 throw new Error(response.status);
+//             }
+//         });
+// }
+//
+// let url = "https://api.github.com/users/yalchin403";
+// loadJson(url)
+//     .then(user => console.log(user.id))
+//     .catch(err => console.log(err)); // Error: 404
+
+// convert it into async
+
+
+async function loadJson(url) {
+    let user = await fetch(url);
+    if(user.status === 200) {
+        return user.json();
+    }
 }
 
 let url = "https://api.github.com/users/yalchin403";
 loadJson(url)
-    .then(user => console.log(user.id))
-    .catch(err => console.log(err)); // Error: 404
-
-// convert it into async
+    .then(
+        user => console.log(user)
+    )
